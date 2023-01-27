5b26db9df53dbdac846e9d254b0c05cd57210c40
unknown <jefferyliu521@gmail.com>
2023-01-26 19:49:50 -0500

    whoops the previous feature was actually F02 also F03 is done with this

--- README.md
+++ README.md
@@ -31,7 +31,7 @@ Remark: **We are assuming here you are using a _real_ shell (e.g., anything but 
 | :-:  |:-:  |---       | :-:     | :-:       | :-:       |
 | x   | F01 | Roll a dice | S | 01/01/23 | 26/01/23 |
 | x   | F02 | Roll eight dice | S | 26/01/23 | 26/01/23 |
-| x   | F03 | Compute number of golds and diamonds | S | 26/01/23 |  |
+| x   | F03 | Compute number of golds and diamonds | S | 26/01/23 | 26/01/23 |
 | x   | F04 | End of game with three skulls | P | |
 | x   | F05 | Player keeping random dice at their turn | B (F02) | | 
 | x   | F06 | Repeat game 42 times, print out result| B (F04) | | 
--- src/main/java/PiratenKarpen.java
+++ src/main/java/PiratenKarpen.java
@@ -1,14 +1,27 @@
 import pk.Dice;
+import pk.Faces;
 
 public class PiratenKarpen {
-
     public static void main(String[] args) {
         System.out.println("Welcome to Piraten Karpen Simulator!");
         System.out.println("I'm rolling eight dice");
+        
+        int numOfGold = 0;
+        int numOfDiamond = 0;
+
         for (int i = 0; i < 8; i ++){
             Dice myDice = new Dice();
-            System.out.println(myDice.roll());
+            Faces roll = myDice.roll();
+            System.out.println(roll);
+            if (roll == Faces.GOLD){
+                numOfGold ++;
+            }
+            else if (roll == Faces.DIAMOND){
+                numOfDiamond ++;
+            }
         }
+        System.out.println("Number of golds: " + numOfGold);
+        System.out.println("Number of diamonds: " + numOfDiamond);
         System.out.println("That's all folks!");
     }
     
